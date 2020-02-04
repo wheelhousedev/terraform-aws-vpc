74d1926e3446d4d34f6534e60efd345cf032af67
Dallas Slaughter <dslaughtr@gmail.com>
2020-02-04 15:02:41 -0800

    [bugfix] - Add a status filter to the peering connection accepter resource

--- variables.tf
+++ variables.tf
@@ -1888,6 +1888,6 @@ variable "atlas_cidr_block" {
 }
 
 variable "atlas_vpc_peering_connection_id" {
-    type = string
-    description = "The peering connection ID (pcx-XXXXX) for the Atlas connection."
+  type        = string
+  description = "The peering connection ID (pcx-XXXXX) for the Atlas connection."
 }
